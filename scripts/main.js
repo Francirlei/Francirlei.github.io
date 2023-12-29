1928a350716a17d9116285f5d98868f574c44f33
Francirlei <francirlei-silva2011@hotmail.com>
2023-12-28 21:07:22 -0300

    Ativando o dia

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const arrowRight = document.getElementById("iconRight");
 const arrowLeft = document.getElementById("iconLeft");
 const containerMonths = document.querySelector(".months");
 const months = document.querySelectorAll(".month");
+const days = document.querySelectorAll(".day");
 
 arrowRight.addEventListener("click", () => {
   containerMonths.scrollLeft += containerMonths.offsetWidth / 2;
@@ -13,7 +14,6 @@ arrowLeft.addEventListener("click", () => {
 
 months.forEach((month) => {
   month.addEventListener("click", (element) => {
-
     for (let m of months) {
       if (m.dataset.monthActive === "true") {
         m.dataset.monthActive = false;
@@ -23,6 +23,22 @@ months.forEach((month) => {
     }
 
     element.target.classList.add("active");
-    element.target.dataset.monthActive = true;
+    element.target.dataset.monthActive = "true";
+  });
+});
+
+days.forEach((day) => {
+  day.addEventListener("click", (element) => {
+    for (let d of days) {
+      if (d.dataset.dayActive === "true") {
+        console.log(d.dataset.dayActive);
+        d.dataset.dayActive = false;
+        d.classList.remove("active");
+        break;
+      }
+    }
+    
+    element.target.classList.add("active");
+    element.target.dataset.dayActive = "true";
   });
 });
